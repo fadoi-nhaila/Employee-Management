9b9335a426bb18193ba0e42a90b69c7e0a0bed24
Your Name <you@example.com>
2022-03-04 12:09:15 +0100

    first commit

---
+++ resources/js/routes.js
@@ -0,0 +1,22 @@
+import EmployeesIndex from "./components/employees/Index.vue";
+import EmployeesCreate from "./components/employees/Create.vue";
+import EmployeesEdit from "./components/employees/Edit.vue";
+
+const routes = [
+	{
+        path: "/employees",
+        name: "EmployeesIndex",
+        component: EmployeesIndex
+    },
+    {
+        path: "/employees/create",
+        name: "EmployeesCreate",
+        component: EmployeesCreate
+    },
+    {
+        path: "/employees/:id",
+        name: "EmployeesEdit",
+        component: EmployeesEdit
+    }
+];
+
